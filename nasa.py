0cdfa50b411d11d9e9d6ab4b35bbbf4967239745
Raphael-Borges <raps_rnb@hotmail.com>
2023-03-16 23:23:42 -0300

    feat: Nasa API

---
+++ nasa.py
@@ -0,0 +1,12 @@
+import requests
+
+# API para consultar astronautas no espaço
+
+# URL da API
+url = 'http://api.open-notify.org/astros.json'
+
+# GET
+
+response = requests.get(url)
+number = str(response.json()['number'])
+print('Número de astronautas no espaço: ' + number)
